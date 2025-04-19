66eea18d8bf3d98c79bf1280465b02d6a168e7c9
pmahdiq <m.esmailzade1377@.com>
2025-04-19 17:23:34 +0330

    add attributes of Student class

--- Firstmove.cpp
+++ Firstmove.cpp
@@ -3,5 +3,34 @@
 using namespace std;
 int main()
 {
-    
+
 }
+class Student
+{
+    int user_id;
+    string student_id,name,email;
+    float balance;
+    bool is_active;
+    public:
+        void setuser_id(int us_id);
+        void setstudent_id(string stu_id);
+        void setname(string name);
+        void setemail(string email);
+        void setbalance(float balance);
+        void setis_active(bool is_act);
+        int getuser_id();
+        int getstudent_id();
+        int getname();
+        int getemail();
+        int getbalance();
+        int getis_active();
+        Student();
+        void print();
+        void reserve_meal(Meal meal);
+        bool cancle_reservation(Reservation res);
+
+};
+
+
+
+
