7d2828d073a382c878ab088ce240ec0c6ce74f4a
Shahrad_365 <Shahradmostofi57@gmail.com>
2025-04-19 17:51:27 +0330

    add private properties

--- Firstmove.cpp.cpp
+++ Firstmove.cpp.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+enum ghaza
+{
+   
+};
 class Meal
 {
+    private:
     int meal_id;
+    string name;
+    float price;
+    enum meal_type
+    {
+
+    };
+    vector <ghaza> side_items;
+    
+
 
 };
 class DiningHall
 {
+    private:
+    int hall_id;
+    string name;
+    string address;
+    int capacity;
 
 };
 int main()
